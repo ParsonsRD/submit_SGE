848eb1b3eb37af0e7719199b819a3f8387e1db41
Dan Parsons <parsonsrd@gmail.com>
2018-09-28 12:00:55 +0200

    Created setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='submit_SGE',
+    version='0.1',
+    packages=['submit_SGE'],
+    url='',
+    license='',
+    author='parsonsrd',
+    author_email='',
+    description='Class to allow submission of shell commands to the SGE'
+)
