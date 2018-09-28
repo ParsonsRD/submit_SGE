c68b8d58a5c61a4de9d5d751a7134fe52bf61cf9
Dan Parsons <parsonsrd@gmail.com>
2018-09-28 11:58:54 +0200

    Created repository and SGE class

---
+++ submit_SGE.py
@@ -0,0 +1,151 @@
+"""
+The purpose of this code is to allow the submission of a large number of shell commands
+to the sun grid engine (SGE). This also allows a maximum number of jobs to be submitted
+and wait until the job number falls below this number.
+"""
+
+import os
+import getpass
+import time
+import subprocess
+
+
+class SubmitSGE:
+
+    def __init__(self, queue_name="", extra_options="", maximum_jobs=100, queue_update=60,
+                 verbose=False):
+        """
+        Class for submitting large number of jobs to the sun grid engine
+
+        :param queue_name: str
+            Name of SGE queue to submit to (uses default if empty)
+        :param extra_options: str
+            Extra option to add to qsub command
+        :param maximum_jobs: int
+            Maximum number of jobs to submit
+        :param queue_update: float
+            Time to wait between refreshes of the queue content
+        :param verbose: bool
+            Set verbosity level
+        """
+        self.queue_name = queue_name
+        self.verbose = verbose
+        self.extra_options = extra_options
+        self.queue_update = queue_update
+        self.maximum_jobs = maximum_jobs
+
+    def get_jobs_in_queue(self):
+        """
+        Get the total number of jobs in the SGE queue
+
+        :return: int
+            Number of total jobs in queue
+        """
+        username = getpass.getuser()
+
+        stat = "qstat -u " + username
+        if self.queue_name is not "":
+            stat += "-q " + self.queue_name
+
+        f = os.popen(stat)
+
+        job_number = len(f.readlines())
+
+        if job_number > 0:
+            job_number -= 2
+        return job_number
+
+    def get_jobs_in_queue_name(self, name_fragment):
+        """
+        Get the number of jobs in the SGE queue containing a name_fragment
+
+        :param name_fragment: str
+            Common name element tto search for
+        :return: int
+            Number of jobs containing fragment
+        """
+        username = getpass.getuser()
+        stat = "qstat -u " + username
+        if self.queue_name is not "":
+            stat += "-q " + self.queue_name
+
+        f = os.popen(stat)
+
+        job_number = 0
+        for i in f.readlines():
+            if i.find(name_fragment) > -1:
+                job_number = job_number + 1
+
+        if self.verbose:
+            print(job_number, "Jobs containing string", name_fragment, "in queue")
+
+        return job_number
+
+    def submit_job(self, command, job_name):
+        """
+        Submit a shell command to the SGE queue
+
+        :param command: str
+            Command to submit
+        :param job_name: str
+            Name of job
+        :return: None
+        """
+        filename = "./submit_" + job_name + ".sh"
+
+        libpath = os.environ.get("LD_LIBRARY_PATH")
+        path = os.environ.get("PATH")
+        pypath = os.environ.get("PYTHONPATH")
+
+        f = open(filename, 'w')
+        f.write("#/bin/sh \n \n")
+        f.write("export LD_LIBRARY_PATH=" + libpath + " \n")
+        f.write("export PATH=" + path + " \n")
+        f.write("export PYTHONPATH=" + pypath + " \n")
+
+        f.write('echo -------------------------------------\n')
+        f.write('echo "USER    : $USER" \n')
+        f.write('echo "JOB_ID  : $JOB_ID" \n')
+        f.write('echo "JOB_NAME: $JOB_NAME" \n')
+        f.write('echo "HOSTNAME: $HOSTNAME" \n')
+        f.write('echo -------------------------------------\n')
+        f.write("ulimit -c 1\n")  # prevent big core dumps!
+
+        f.write(command)
+
+        f.close()
+
+        call_command = ["qsub", self.extra_options, "-notify", "-N", job_name, filename]
+
+        subprocess.call(call_command)
+
+        time.sleep(1)
+
+        os.remove(filename)
+
+    def submit_job_when_ready(self, command, job_name):
+        """
+        Submit a job to the SGE queue when the number of submitted jobs is below the
+        maximum allowed
+
+        :param command: str
+            Command to submit
+        :param job_name: str
+            Name of job
+        :return: None
+        """
+        job_number = self.get_jobs_in_queue()
+        if self.verbose:
+            print(job_number, "out of", self.maximum_jobs,
+                  "allowed jobs currently submitted")
+
+        while True:
+            job_number = self.get_jobs_in_queue()
+
+            if job_number < self.maximum_jobs:
+                if self.verbose:
+                    print("Submitting Job", job_name)
+                self.submit_job(command, job_name)
+                break
+            else:
+                time.sleep(self.queue_update)
